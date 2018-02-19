4a3882c7197150f8f1d15cdb77f6dff5e368ef18
carloscastanedan <carloscastanedan@gmail.com>
2018-02-19 10:44:15 -0600

    Se crea al middleware de autorización

---
+++ src/server/middlewares/auth.js
@@ -0,0 +1,20 @@
+import services from '../services/token'
+
+function isAuth(req, res, next)
+{
+  if(!req.headers.authorization)
+  {
+    return res.status(403).send({ message: ' No tiene autorización '})
+  }
+
+  const token = req.headers.authorization.split(' ')[1]
+
+  services.decodeToken(token)
+    .then(response => {
+      req.user = response
+      next()
+    })
+    .cath(response => {
+      res.status(response.status)
+    })
+}
