b1de64f090c8df08cd1fa89b0e439fa064576297
carloscastanedan <carloscastanedan@gmail.com>
2018-02-19 09:50:28 -0600

    Se crea archivo de configuracion general config.js, se configura la comunicación con la base de datos

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,22 @@
-import app from './app'
+//Dependencies
 import open from 'open'
+import mongoose from 'mongoose'
+//Asets
+import app from './app'
+import config from '../../config'
 // Envía el trafico entrante a React
-app.get('*', (req, res) => {
-  res.sendFile('index.html')
- });
 
- // Escucha el puerto 3000
- app.listen(3000, err => {
-   if (!err) {
-     open('http://localhost:3000')
-   }
- });
+
+ // Conexion con DB y corre el servidor
+mongoose.connect(config.db, (err, res) => {
+  if(err)
+    return console.log(`Error al conectar con la base de datos:${err}`)
+
+  console.log('Conexión con la base de datos establecida')
+  app.listen(config.port, err => {
+    if (!err) {
+      open(`http://localhost:${config.port}`)
+    }
+  })
+})
+
