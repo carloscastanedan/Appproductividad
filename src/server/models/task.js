058c3cbdca287e0f6bf42429ba61bd69a407656f
carloscastanedan <carloscastanedan@gmail.com>
2018-02-19 10:02:16 -0600

    Se crean modelos de mongoose

---
+++ src/server/models/task.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+const TaskSchema = new Schema({
+  name: String,
+  description: String,
+  active: Boolean,
+  color: String,
+  duration: { type: Date, default: 0 }
+})
+
+module.export = mongoose.model('Todo', TaskSchema)
---
+++ src/server/models/user.js
@@ -0,0 +1,27 @@
+//Dependencies
+import mongoose, { Schema } from 'mongoose'
+import bcrypt from 'bcrypt-nodejs'
+
+const UserSchema = new Schema({
+  email: { type: String, unique: true, lowercase: true },
+  displayName: String,
+  password: { type: String, select: false },
+  signUpDate: { type: Date, default: Date.now() },
+  lastLogin: Date
+})
+
+UserSchema.pre('save', (next) => {
+  let user = this
+  if(!user.isModified('password')) return next
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if(err) return next(err)
+
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      user.password = hash
+      next()
+    })
+  })
+})
+
+module.exports = mongoose.model('User', UserSchema)
